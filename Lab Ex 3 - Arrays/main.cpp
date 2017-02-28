ec5e9cc3c372d1215152307673c34266ab3e636a
Harp <200090654@fvtc.edu>
2017-02-28 12:36:09 -0600

    Finished Assignment

--- Lab Ex 3 - Arrays/main.cpp
+++ Lab Ex 3 - Arrays/main.cpp
@@ -3,43 +3,85 @@
 // Instructions:
 /*
 	[ ] Write a program that performs the following:
-		[ ] Prompt the user to input five numbers.
-		[ ] Store the input into either an array or a vector.
-		[ ] Once the user is finished entering numbers:
-			[ ] Display the five numbers in reverse order.
-			[ ] Display the highest number entered.
-			[ ] Display the lowest number entered.
-		[ ] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
-	[ ] Compress(zip) your program and submit it to Blackboard.
+		[x] Prompt the user to input five numbers.
+		[x] Store the input into either an array or a vector.
+		[x] Once the user is finished entering numbers:
+			[x] Display the five numbers in reverse order.
+			[x] Display the highest number entered.
+			[x] Display the lowest number entered.
+		[x] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
+	[x] Compress(zip) your program and submit it to Blackboard.
 */
 
+#include <vector>
 #include <iostream>
 #include <conio.h>
+
 
 using namespace std;
 
 int main()
 {
-	int numbers[5];
+	vector<int> arr;
+	int size = sizeof(arr) / sizeof(int);
 	int x;
 
-	/*int a;
-	int b;
-	int c;
-	int d;
-	int e;*/
+	char result;
 
-	cout << "Enter 5 numbers: ";
-	while (int i < 5)
+
+	for (int i = 0; i < 5; i++)
 	{
+		cout << "Enter a number: ";
+		cin >> x;
+		arr.push_back(x);
+		//arr[i] = x;
 
 	}
 
-	int arr[5] = {a, b, c, d, e };
+	
+	do {
+		cout << "do you want to add another number? y/n: ";
+		cin >> result;
+		if (result == 'y' || result == 'Y')
+		{
+			cout << "Enter a number: ";
+			cin >> x;
+			arr.push_back(x);
+			size++;
+		}
+		
+	} while (result == 'y' || result == 'Y');
 
-	//cout << arr[e] << arr[d] << arr[c] << arr[b] << arr[a];
+	int index = size;
+	cout << size << endl;
+	for (int i = 0; i <= size ; i++)
+	{
+		
+		cout << arr[index] << ", ";
+		index--;
+	}
 
+	cout << endl;
 
+	int max = 0;
+	for (int i = 0; i <= size; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	cout << "Highest number is: " << max << endl;
+	int min = max;
+	for (int i = 0; i <= size; i++)
+	{
+
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+	cout << "Lowest number is: " << min;
 
 	_getch();
 	return 0;
